47a6107670453897e1b3fa6a506c5095d505e40d
junhyoung2 <jun83467@gmail.com>
2025-06-13 14:27:31 +0900

    0613 v2

--- src/App.js
+++ src/App.js
@@ -1,10 +1,54 @@
+import "./App.scss";
+import { useState } from "react";
+import mockData from "./Data/mockdata";
+import Category from "./Components/Category";
+import FlashCard from "./Components/FlashCard";
+import CardLearn from "./Components/CardLearn";
+import Result from "./Components/Result";
 
 const App = () => {
-  return (
-    <div>
-      
-    </div>
-  );
+    const [category, setCategory] = useState(null);
+    const [learningMode, setLearningMode]=useState(false);
+    const [exitMode, setExitMode]=useState(false);
+    const handleCategory = (obj) => {
+        setCategory(obj);
+    };
+    const handleLearn  = (data)=>{
+      setLearningMode(data);
+    }
+    const handleExit =(mode)=>{
+      setExitMode(mode);
+    }
+    return (
+        <div className="app">
+            {!category && (
+                <Category
+                    cardData={mockData.categories}
+                    onSelected={handleCategory}
+                />
+            )}
+            {category && !learningMode &&
+             <FlashCard
+             cardData={category} 
+            onSelected={handleCategory} 
+            onLearn={handleLearn}
+            />}
+            {
+              learningMode && !exitMode &&
+              <CardLearn cardData={category.flashcards}
+              onExit={handleExit}
+              />
+       }
+       {
+        exitMode &&
+        <Result onExit={()=>{
+        setCategory(null);
+        setLearningMode(false);
+        setExitMode(false);
+        }}/>
+       }
+        </div>
+    );
 };
 
 export default App;
